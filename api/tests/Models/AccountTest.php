4175f4931b6a6beefb24f71a5667aeec5a244074
Tim GIMENEZ <tim.gimenez26@gmail.com>
2022-03-19 16:25:49 +0100

    feat: init accounttest

---
+++ api/tests/Models/AccountTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Models;
+
+use App\Models\Account;
+use PHPUnit\Framework\TestCase;
+
+class AccountTest extends TestCase
+{
+
+    public function testSetCurrentBalanceAmount()
+    {
+
+    }
+
+    public function testGetThreeTransactions()
+    {
+
+    }
+
+    public function testLatestBalances()
+    {
+
+    }
+
+    public function testRefreshData()
+    {
+
+    }
+
+    public function testTransactionsForActualMonth()
+    {
+
+    }
+
+    public function testInitBalance()
+    {
+
+    }
+
+    public function testTotaltransactionsForActualMonth()
+    {
+
+    }
+
+    public function testSetLatestTransactions()
+    {
+
+    }
+}
